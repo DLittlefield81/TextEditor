2be31771f4c18958b50b95dc134a5e0bdae70d3d
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-07-15 18:20:45 -0400

    working on webpackconfig

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,29 +2,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Jate',
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest:'./src-sw.js',
+    }),
+    new MiniCssExtractPlugin(),
+    new WebpackPwaManifest.GenerateSW()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
